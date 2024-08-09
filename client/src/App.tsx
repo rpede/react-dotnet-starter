5de90da2280845b29fdf2b1d7fbebe1ed210986f
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-08-09 17:19:00 +0200

    Component for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Api, LoginRequest, WeatherForecast } from "./api";
+import { AxiosResponse } from "axios";
+
+const api = new Api();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [credential, setCredential] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+
+  const [status, setStatus] = useState<string | null>(null);
+  const [data, setData] = useState<WeatherForecast[]>([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  async function loadData() {
+    try {
+      const response = await api.weatherForecast.getWeatherForecast();
+      setStatus(response.statusText);
+      setData(response.data);
+    } catch (e) {
+      const error = e as AxiosResponse;
+      setStatus(error.statusText);
+      setData([]);
+    }
+  }
+
+  async function login() {
+    const response = await api.login.loginCreate(credential, {
+      useCookies: true,
+    });
+    loadData();
+  }
+
+  async function register() {
+    await api.register.registerCreate(credential);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+                onChange={(e) =>
+                  setCredential({ ...credential, email: e.target.value })
+                }
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                onChange={(e) =>
+                  setCredential({ ...credential, password: e.target.value })
+                }
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary" onClick={() => register()}>
+                Register
+              </button>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-secondary" onClick={() => login()}>
+                Login
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-title">{status}</div>
+            <div className="card-body">{JSON.stringify(data)}</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
